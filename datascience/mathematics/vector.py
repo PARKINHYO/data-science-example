09331fdf846f02443bf89a87394d909fdd1e0d7d
parkinhyo <inhyopark122@gmail.com>
2020-08-27 00:11:35 +0900

    mod viector.py

--- datascience/mathematics/vector.py
+++ datascience/mathematics/vector.py
@@ -1,42 +1,139 @@
-from typing import List
+from typing import List, Tuple, Callable
+import math
+
 
 def func(a: str, b: float = 3.5) -> int:
+    return a + b
 
-    return a+b
 
 value = func(3)
 print(value)
 
 Vector = List[float]
 
-#fixme inches, pounds, yesars
+# fixme inches, pounds, yesars
 height_weight_age = [79, 170, 40]
 
 grades = [95, 80, 75, 62]
 
-def add(v: Vector, w: Vector) -> Vector:
 
+def add(v: Vector, w: Vector) -> Vector:
     assert len(v) == len(w), "Vectors must be the same length"
 
     return [v_i + w_i for v_i, w_i in zip(v, w)]
 
+
 assert add([1, 2, 3], [4, 5, 6]) == [5, 7, 9]
 
-def subtract(v: Vector, w: Vector) -> Vector:
 
+def subtract(v: Vector, w: Vector) -> Vector:
     assert len(v) == len(w), "Vectors must be the same length"
 
     return [v_i - w_i for v_i, w_i in zip(v, w)]
 
+
 assert subtract([5, 7, 9], [4, 5, 6]) == [1, 2, 3]
+
 
 def vector_sum(vectors: List[Vector]) -> Vector:
     answer = vectors.pop()
-    while(len(vectors)>0):
+    while (len(vectors) > 0):
         answer = add(answer, vectors.pop())
 
     return answer
 
+
 assert vector_sum([[1, 2], [3, 4], [5, 6], [7, 8]]) == [16, 20], "vector_sum error"
 
 
+def scalar_multiply(c: float, v: Vector) -> Vector:
+    """Multiplies every element by c"""
+    return [c * v_i for v_i in v]
+
+
+def vector_mean(vectors: List[Vector]) -> Vector:
+    """Computers the element-wise average"""
+    n = len(vectors)
+    return scalar_multiply(1 / n, vector_sum(vectors))
+
+
+assert vector_mean([[1, 2], [3, 4], [5, 6]]) == [3, 4]
+
+
+def dot(v: Vector, w: Vector) -> float:
+    """Computes v_1 * w_1 + ... + v_n * w_n"""
+    assert len(v) == len(w), "vectors must be smame length"
+
+    return sum(v_i * w_i for v_i, w_i in zip(v, w))
+
+
+assert dot([1, 2, 3], [4, 5, 6]) == 32  # fixme 1 * 4 + 2* 5 + 3* 6
+
+
+def sum_of_squares(v: Vector) -> float:
+    """Returns v_1 * v_1 + ... + v_n * v_n"""
+    return dot(v, v)
+
+
+assert sum_of_squares([1, 2, 3]) == 14  # 1 * 1 + 2 * 2 + 3 * 3
+
+
+def magnitude(v: Vector) -> float:
+    """Returns the magnitude (or Length) v"""
+    return math.sqrt(sum_of_squares(v))
+
+
+assert magnitude([3, 4]) == 5
+
+
+def squared_distance(v: Vector, w: Vector) -> float:
+    """Computes (v_1 - w_1) ** 2 + ... + (v_n - w_n) ** 2"""
+    return sum_of_squares(subtract(v, w))
+
+
+def distance(v: Vector, w: Vector) -> float:
+    """Computes the distance between v and w"""
+    return math.sqrt(squared_distance(v, w))
+
+
+def distance2(v: Vector, w: Vector) -> float:
+    """Computes the distance between v and w"""
+    return magnitude(subtract(v, w))
+
+
+Matrix = List[List[float]]
+
+A = [[1, 2, 3, ], [4, 5, 6]]
+B = [[1, 2], [3, 4], [5, 6]]
+
+
+def shape(A: Matrix) -> Tuple[int, int]:
+    return (len(A), len(A[0]) if A else 0)
+
+
+assert shape([[1, 2, 3], [4, 5, 6]]) == (2, 3)
+
+
+def get_row(A: Matrix, i: int) -> Vector:
+    """#fixme Returns the i-th row of A(as a Vector)"""
+    return A[i]  # fixme A[i] is already the ith row
+
+
+def get_column(A: Matrix, j: int) -> Vector:
+    """Returns the j-th column of A(as a Vector"""
+    return [A_i[j] for A_i in A]  # fixme ith element of row A_i for each row A_i
+
+
+def make_matrix(num_rows: int, num_cols: int, entry_fn: Callable[[int, int], float]) -> Matrix:
+
+    return  [[entry_fn(i, j) for j in range(num_cols)] for i in range(num_rows)]
+
+def identity_matrix(n: int) -> Matrix:
+    """Returns the n x n identity matrix"""
+    return make_matrix(n, n, lambda i, j: 1 if i == j else 0)
+
+assert identity_matrix(5) == [[1, 0, 0, 0, 0],
+                              [0, 1, 0, 0, 0],
+                              [0, 0, 1, 0, 0],
+                              [0, 0, 0, 1, 0],
+                              [0, 0, 0, 0, 1]]
